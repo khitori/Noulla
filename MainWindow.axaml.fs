7c70d39bd8486852f54ad5713bf409905c19a818
KHITORI <uhequilza@yandex.ru>
2026-02-08 14:25:02 +0400

    parser upd

--- MainWindow.axaml.fs
+++ MainWindow.axaml.fs
@@ -1,9 +1,12 @@
 namespace Noulla
 
+open Noulla.modules
+open Parser
 open System
 open System.IO
 open Avalonia
 open Avalonia.Controls
+open Avalonia.Interactivity
 open Avalonia.Markup.Xaml
 open System.Collections.Generic
 open Avalonia.Media.Imaging
@@ -11,9 +14,13 @@ open Avalonia.Platform
 
 
 
+
+
 type MainWindow() as this =
     inherit Window ()
 
+    
+    
     let _backgrounds = Dictionary<string, Bitmap>()
     let _characters = Dictionary<string, Bitmap>()
     
@@ -23,20 +30,128 @@ type MainWindow() as this =
             "characters", (_characters, "Assets/Characters")
         ]
     
+    let mutable story: Story = Map.empty
+    let mutable currentCommands: Command list = []
+    let mutable currentIndex = 0
+    
+    let loadStoryJson() =
+        let uri: Uri = Uri("avares://Noulla/Content/story.json")
+        use stream = AssetLoader.Open(uri)
+        use reader = new StreamReader(stream)
+        reader.ReadToEnd()
+    
+    
     
     
     do
         this.InitializeComponent()
-        //this.StretchWindowResolution()
+        this.StretchWindowResolution()
         this.StretchBackgroundImage()
-        this.SetBackground("bg1.jpg")
         this.SetCharacter("char1.png", "left")
-        this.SetTitleText("TEST")
-        this.SetBodyText("TEST")
-
-        let allButtons = ["left"; "center"; "right"]
-        allButtons |> List.iter (fun f -> this.SetButtonText("TEST", f))
+        
+        
+        //this.SetBackground(this.GetBitmap("backgrounds", "bg1.jpg"))
+        
         //this.ClearCharacter("left")
+        //this.SetTitleText("TEST")
+        //this.SetBodyText("TEST")
+
+        //let allButtons = ["left"; "center"; "right"]
+        //allButtons |> List.iter (fun f -> this.SetButtonText("TEST", f))
+    
+        this.LoadStory()
+    
+    
+    
+    
+    
+    
+    member this.LoadStory() =
+        let json = loadStoryJson()
+        story <- Parse(json)
+        currentCommands <- story.["start"]
+        currentIndex <- 0
+        this.ExecuteNextCommand()
+    
+    member private this.ExecuteNextCommand() =
+        if currentIndex < currentCommands.Length then
+            
+            let cmd = currentCommands.[currentIndex]
+            
+            match cmd with
+            | Background img ->
+                let bitmap = this.GetBitmap("backgrounds", img)
+                this.SetBackground(bitmap)
+            | CharacterShow (one, two) ->
+                printfn "character show"
+            | CharacterClear pos ->
+                printfn "character clear"
+            | TitleShow txt ->
+                printfn "text"
+            | TitleClear ->
+                printfn "text"
+            | TextShow txt ->
+                this.SetTitleText("tst")
+            | TextClear ->
+                printfn "text"
+            | ChoiceShow (id, txt, save, next) ->
+                printfn "txt"
+            | ChoiceHide id ->
+                printfn "txt"
+            | WaitClick ->
+                ()
+            | WaitChoice ->
+                ()
+            | Goto sceneId ->
+                currentCommands <- story.[sceneId]
+                currentIndex <- 0
+                this.ExecuteNextCommand()
+            | Save data ->
+                printfn "data"
+
+            match cmd with
+            | WaitClick | WaitChoice -> ()
+            | _ -> currentIndex <- currentIndex + 1
+    
+    
+    
+    
+    
+    // Get Bitmap from Dictionary
+    member private this.GetBitmap(key: string, filename: string) =
+        match paths.TryFind key with
+        | Some (dict, basePath) ->
+            match dict.TryGetValue(filename) with
+            | true, bitmap -> bitmap
+            | false, _ ->
+                let uri: Uri = Uri($"avares://Noulla/{basePath}/{filename}")
+                let bitmap = new Bitmap(AssetLoader.Open(uri))
+                dict[filename] <- bitmap
+                bitmap
+        | None ->
+            failwithf $"Unknown key: {key}"
+    
+    
+    
+    
+
+    member this.TextClick(sender: obj, e: Avalonia.Input.PointerPressedEventArgs) =
+        this.ClearCharacter("left")
+        this.ClearCharacter("center")
+        this.ClearCharacter("right")
+        e.Handled <- true
+        
+    member this.ButtonChoiceLeft(sender: obj, e: RoutedEventArgs) =
+        this.SetCharacter("char1.png", "left")
+    
+    member this.ButtonChoiceCenter(sender: obj, e: RoutedEventArgs) =
+        this.SetCharacter("char1.png", "center")
+    
+    member this.ButtonChoiceRight(sender: obj, e: RoutedEventArgs) =
+        this.SetCharacter("char1.png", "right")
+    
+    
+    
     
     
     
@@ -61,23 +176,9 @@ type MainWindow() as this =
         this.FindControl<Image>("Background").Width <- width
         this.FindControl<Image>("Background").Height <- height
         
-    // Get Bitmap from List of paths
-    member private this.GetBitmap(key: string, filename: string) =
-        match paths.TryFind key with
-        | Some (dict, basePath) ->
-            match dict.TryGetValue(filename) with
-            | true, bitmap -> bitmap
-            | false, _ ->
-                let uri: Uri = Uri($"avares://Noulla/{basePath}/{filename}")
-                let bitmap = new Bitmap(AssetLoader.Open(uri))
-                dict[filename] <- bitmap
-                bitmap
-        | None ->
-            failwithf $"Unknown key: {key}"
-        
     // Set Background from Dictionary
-    member private this.SetBackground(background: string) =
-        this.FindControl<Image>("Background").Source <- this.GetBitmap("backgrounds", background)
+    member private this.SetBackground(bitmap: Bitmap) =
+        this.FindControl<Image>("Background").Source <- bitmap
         
     // Set Character from Dictionary
     member private this.SetCharacter(character: string, position: string) =
